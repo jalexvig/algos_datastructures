748c75eea2e209675ec86b559b3db966c13bc4ec
Alex Vig <jalexvig@gmail.com>
2018-06-09 23:45:08 -0400

    Updated kruskal docstring

--- graphs/kruskal.py
+++ graphs/kruskal.py
@@ -6,6 +6,10 @@ Summary:
     Until a spanning tree is reached get edge with smallest weight. If this edge connects separate components, add it to
     the spanning tree. Otherwise discard it.
 
+    Correctness (induction): Get min weight edge e connecting a, b. If e is not part of a MST, then c connects a, b
+    (through edges f, g) but f + g >= (e + f) or (e + g) since e is minimal. So can replace (f, g) with (e, f) or (e, g)
+    and e now part of a MST. Base case of empty set of edges is true since the empty set in any MST.
+
 Characteristics:
 
     * n number nodes
